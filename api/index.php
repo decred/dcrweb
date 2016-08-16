d8919396f34539b465a8723409d154daf52dcd3c
jolan <jolan@users.noreply.github.com>
2016-08-16 11:21:09 -0500

    add custom download counter that sums both repo's github stats (#14)

--- api/index.php
+++ api/index.php
@@ -78,7 +78,13 @@ case "cc":
         print "unauthorized\n";
     }
     break;
-// get insight status;
+// downloads image cache
+case "dic":
+    header("Content-type: image/svg+xml");
+    $svg = downloadsImageCache();
+    print $svg;
+    break;
+// get insight status
 case "gis":
     $status = getInsightStatus();
     print $status;
@@ -103,6 +109,91 @@ case "gsd":
 function debugLog($s) {
     if ($GLOBALS["debug"]) {
         error_log($s);
+    }
+}
+
+function downloadsFetchURL($url) {
+    $timeOut = 5;
+    $c = curl_init($url);
+    curl_setopt($c ,CURLOPT_USERAGENT, "decred/dcrweb bot");
+    curl_setopt($c, CURLOPT_CONNECTTIMEOUT, $timeOut);
+    curl_setopt($c, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($c, CURLOPT_TIMEOUT, $timeOut);
+    $r = curl_exec($c);
+    if ($r === false) {
+        error_log("curl error: " . curl_error($c) . " (errno: " . curl_errno($c)  . ") while scraping {$url}");
+    }
+
+    return $r;
+}
+
+function downloadsGetCount() {
+    $decred_binaries = downloadsFetchURL("https://api.github.com/repos/decred/decred-binaries/releases");
+    $decred_release = downloadsFetchURL("https://api.github.com/repos/decred/decred-release/releases");
+
+    if (!json_decode($decred_binaries, true)) {
+        error_log("decred_binaries was not valid JSON: '{$decred_binaries}'");
+        exit;
+    }
+
+    if (!json_decode($decred_release, true)) {
+        error_log("decred_release was not valid JSON: '{$decred_release}'");
+        exit;
+    }
+
+    $dataset = array(
+        "db" => json_decode($decred_binaries, true),
+        "dr" => json_decode($decred_release, true),
+    );
+
+    $count = 0;
+
+    foreach (array_values($dataset) as $data) {
+        foreach (array_values($data) as $release) {
+            foreach (array_values($release) as $stats) {
+                if (is_array($stats)) {
+                    foreach ($stats as $id => $keys) {
+                        if (is_array($keys)) {
+                            $file = "";
+                            foreach ($keys as $k => $v) {
+                                if ($k == "name") {
+                                    $file = $v;
+                                }
+                                if ($k == "download_count") {
+                                    //print "$file => $v\n";
+                                    $count = $count + $v;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        //error_log("count $count");
+    }
+
+    return $count;
+}
+
+// XXX will need adjusting when integer count reaches 6 digits
+function downloadsImageCache() {
+    $cacheTTL = 4 * 60 * 60;
+    $svgTemplate = '<svg xmlns="http://www.w3.org/2000/svg" width="128" height="20"><linearGradient id="b" x2="0" y2="100%"><stop offset="0" stop-color="#bbb" stop-opacity=".1"/><stop offset="1" stop-opacity=".1"/></linearGradient><mask id="a"><rect width="128" height="20" rx="3" fill="#fff"/></mask><g mask="url(#a)"><path fill="#555" d="M0 0h69v20H0z"/><path fill="#4c1" d="M69 0h59v20H69z"/><path fill="url(#b)" d="M0 0h128v20H0z"/></g><g fill="#fff" text-anchor="middle" font-family="DejaVu Sans,Verdana,Geneva,sans-serif" font-size="11"><text x="34.5" y="15" fill="#010101" fill-opacity=".3">downloads</text><text x="34.5" y="14">downloads</text><text x="97.5" y="15" fill="#010101" fill-opacity=".3">__COUNT__ total</text><text x="97.5" y="14">__COUNT__ total</text></g></svg>';
+
+    $curSVG = apcu_fetch("downloadsSVG");
+    if (empty($curSVG)) {
+        $count = downloadsGetCount();
+        if (!$count) {
+            error_log("downloadsGetCount returned '{$count}'");
+            exit;
+        }
+        $countString = round($count / 1000) . "k";
+
+        $newSVG = str_replace("__COUNT__", $countString, $svgTemplate);
+        apcu_store("downloadsSVG", $newSVG, $cacheTTL);
+        return $newSVG;
+    } else {
+        return $curSVG;
     }
 }
 
