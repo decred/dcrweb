2a0365e6aa10ee648684827be9b65d8c7ce17017
Mykolas <mykolas.raizys@gmail.com>
2018-06-15 11:40:55 +0300

    Press page accordion effect improvement

--- www-root/content/js/main.js
+++ www-root/content/js/main.js
@@ -18,9 +18,27 @@ $(document).ready(function () {
 	});
 
 	$('.press__releases-item').on('click', function() {
-		$('.press__releases-item').toggleClass(active);
+		$a = $(this);
+
+		$('.press__releases-item').not(this).removeClass('active');
+		$(this).toggleClass('active');
+
+		$('.press__releases-item').not(this).find('.press__releases-arrow').removeClass('active');
 		$(this).find('.press__releases-arrow').toggleClass('active');
-		$(this).next('.press__releases-content').slideToggle();
+
+		$('.press__releases-content').slideUp();
+
+		if($(this).next('.press__releases-content').is(":visible")) {
+			$(this).next('.press__releases-content').slideUp();
+		} else {
+			$(this).next('.press__releases-content').slideToggle( "slow", function(){
+			if ($(this).is(":visible")) { 
+				$('html,body').animate({ 
+					scrollTop: $(this).offset().top - 100
+				}, "slow")
+			}
+		});
+		}
 	});
 
 	// for json API
