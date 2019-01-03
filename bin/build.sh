f506b91bbc13f96317f37f1e06b0adc7aeb43ba7
Peter Banik <peter@froggle.org>
2019-01-03 22:15:28 +0100

    Docker image name defaults to decred/dcrweb

--- bin/build.sh
+++ bin/build.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
+IMAGE_NAME=decred/dcrweb
 
-docker build -t peterzen/dcrweb-hugo-builder .
+if [ "$1" != "" ]; then
+    IMAGE_NAME=$1
+fi
+
+docker build -t $IMAGE_NAME  .
 
 
