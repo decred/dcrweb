21d47b5665d5ad45be4bcbcede142eee6d558fbf
Peter Banik <peter@froggle.org>
2019-03-19 14:07:27 +0100

    Self-hosted intro video in homepage modal (#604)

--- src/assets/js/mediaelement.js
+++ src/assets/js/mediaelement.js
@@ -6,24 +6,24 @@ function initMediaElement(onSuccessCallback) {
         videoWidth: '100%',
         videoHeight: '100%',
         enableAutosize: true,
-        alwaysShowControls: true,
+        alwaysShowControls: false,
         autoRewind: true,
         hideVolumeOnTouchDevices: true,
         success: function (media) {
             var renderer = document.getElementById(media.id + '-rendername');
 
             media.addEventListener('loadedmetadata', _.once(function () {
-                var src = media.originalNode.getAttribute('src').replace('&amp;', '&');
+                var src = media.originalNode.getAttribute('src');
                 if (renderer && src) {
                     renderer.querySelector('.src').innerHTML = '<a href="' + src + '" target="_blank" rel="noopener noreferrer">' + src + '</a>';
                     renderer.querySelector('.renderer').innerHTML = media.rendererName;
                     renderer.querySelector('.error').innerHTML = '';
                 }
-                onSuccessCallback(media);
             }));
             media.addEventListener('error', function (e) {
                 renderer.querySelector('.error').innerHTML = '<strong>Error</strong>: ' + e.message;
             });
+            onSuccessCallback(media);
         }
     });
 }
