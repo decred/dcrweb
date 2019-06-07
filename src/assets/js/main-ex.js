e843023de7ddce2c24b0f7887286947d48b5b57b
Jujhar <jujhar.pannu@gmail.com>
2019-06-06 17:24:15 -0700

    Update exchanges buttons and filteration
    Move category selection to top
    
    Create section links in exchanges category selection
    
    Add url exchange selection highlight
    
    Display selected exchanges filter beside subheader
    
    Make exchange filter selection responsive
    
    Add ability to save selected exchange category state in url
    
    Split exchanges buttons layout
    
    Update categories layout with spacing changes
    
    Directly match exchanges filter tab style with contributors tabs
    
    Add flash in Exchange category changes
    
    Remove console log calls from Exchange category scripts
    
    Add cursor to all Exchange category links
    
    Unify Exchanges page dynamic URL parameters
    
    Refactor Exchanges section JavaScript
    
    Add link functionality with javascript disabled + hide exchange buttons
    
    Unify Exchanges page dynamic URL parameters
    
    Refactoring exchanges related CSS
    
    Refactor exchanges item click bookmark logic
    
    Add back Exchange selection sections
    
    Adding back new code
    
    Remove labelling bug on Exchanges page load

--- src/assets/js/main-ex.js
+++ src/assets/js/main-ex.js
@@ -1,16 +1,38 @@
 $(function(){
+
+    // retrieve category by selected get params
+    function preloadSelection(select) {
+      $( document ).ready(function() {
+        $("a[data-group='" + select + "']").click();
+      });
+    }
+
+    let items = [
+      'high-liquidity',
+      'instant-exchange',
+      'fiat',
+      'decentralized',
+      'atomic-swaps'
+    ];
+
+    items.forEach(function(item) {
+      if (window.location.href.includes(item)) {
+        preloadSelection(item);
+      }
+    });
+
     // default variables
     var tail = $('.tail'),
-      filterCard = $('.filter-card'),
-      filters = $('.ex-filter').children(),
-      filter = $('.filter-button'),
-      all = $('.all'),
-      currentFilter = '';
+
+    filterCard = $('.filter-card'),
+    filters = $('.ex-filter').children(),
+    filter = $('.filter-button'),
+    all = $('.all'),
+    currentFilter = '';
 
     if (location.hash && location.hash.length) {
         currentFilter = decodeURIComponent(location.hash.substr(1));
     }
-
 
     var $grid = $('.filter-cards-wrap');
         //$sizer = $grid.find('.shuffle__sizer');
@@ -51,6 +73,18 @@ $(function(){
         filters.removeClass('is-checked is-hover');
         filters.siblings('[data-group="'+group+'"]').addClass('is-checked');
 
+        // Display selected filter in exchanges-filter subheader neatly
+        var output = ""
+        if (
+          group !== "all" &&
+          group !== "pp-sec" &&
+          group !== "ex-sec" &&
+          group !== "otc-traders") {
+              output = "(" + group + ")";
+              output = output.replace('-',' ');
+        }
+        $('#exchanges-filter').html(output);
+
         $('.roadmap__filter-legend-item').removeClass('is-checked');
         $(".roadmap__filter-legend-item[data-group='" + group +"']").addClass('is-checked');
 
@@ -58,7 +92,7 @@ $(function(){
         window.location.hash = group;
     }
 
-    // header filter buttons 
+    // header filter buttons
     filters.on('click', function() {
         var group = $(this).attr('data-group');
         setCurrentFilter(group);
@@ -79,7 +113,7 @@ $(function(){
 
     // shuffle js .filter-card hover fix
     $grid.on('layout.shuffle', function() {
-        setTimeout(function() {  
+        setTimeout(function() {
             filterCard.each(function() {
                 var style = $(this).attr('style');
                 $(this).attr('style', style+' -webkit-transition: all 200ms ease !important; transition: all 200ms ease !important;');
@@ -87,12 +121,13 @@ $(function(){
         }, 50);
     });
     filterCard.on('mouseenter', function() {
-        $(this).css('margin-top', '-5px');       
+        $(this).css('margin-top', '-5px');
     }).on('mouseleave', function() {
         $(this).css('margin-top', '0px');
     });
 
     $('#hide-all').click(function(){
+        $('#exchanges-filter').html("");
         $('#x-menu').toggle(500);
         $('#hide-all').hide();
         $('#show-all').show();
@@ -100,6 +135,7 @@ $(function(){
     });
 
     $('#show-all').click(function(){
+        $('#exchanges-filter').html("");
         $('#x-menu').toggle(500);
         $('#show-all').hide();
         $('#up-down').hide();
@@ -121,7 +157,7 @@ $(function(){
     var sections = $('.panelSection');
 
     $('a.section-target').click(function(){
-        $('html,body').animate({ 
+        $('html,body').animate({
             scrollTop: $('.panelSection#' + $(this).data('section')).offset().top - 10
         }, "slow")
     });
@@ -132,7 +168,7 @@ $(function(){
           entered: function(direction) {
             $('#x-menu li').removeClass('active');
             $('#x-menu li a[data-section="' + this.element.id +'"]').parent().addClass('active');
-            console.log(this.element.id + ' entered');
+            // console.log(this.element.id + ' entered');
             var index = $( '.panelSection' ).index( $('#' + this.element.id) );
             var total = $( '.panelSection' ).length;
             if(index < total - 1) {
@@ -152,3 +188,76 @@ $(function(){
        })
     })
 });
+
+// tabs on exchanges page
+function exchangesClick(){
+  $('#exchanges').show();
+  $('#exchanges-header')
+      .fadeOut(1)
+      .html($("#ex-header").html());
+  $('#exchanges-desc').html($("#ex-desc").html());
+  $('#exchanges-header')
+      .fadeIn(155);
+  $('#exchanges-desc')
+      .fadeOut(1)
+      .fadeIn(155);
+  $('#exchanges-filter')
+      .fadeOut(1)
+      .fadeIn(155);
+  $('#exchanges-sec')
+      .fadeOut(1)
+      .fadeIn(155);
+  $('#ex-filter')
+      .fadeOut(1)
+      .fadeIn(155);
+  $('#ex-filter').show();
+  $('#exchanges-filter').show();
+  $('#exchanges-sec').show();
+  $('#otc-sec').hide();
+  $('#pp-sec').hide();
+  $('#pp-providers').hide();
+  $('#otc-providers').hide();
+}
+
+function otcClick(){
+  $('#exchanges-desc').html($("#otc-desc").html());
+  $('#exchanges-header').html($("#otc-traders").html());
+  flicker();
+  $('#otc-sec').hide();
+  $('#ex-filter').hide();
+  $('#exchanges-filter').hide();
+  $('#payment-processors').hide();
+  $('#payment-desc').hide();
+  $('#exchanges-sec').hide();
+  $('#pp-sec').hide();
+  $('#pp-providers').hide();
+  $('#otc-providers').show();
+}
+
+function ppClick(){
+  $('#exchanges-header').html($("#payment-processors").html());
+  $('#exchanges-desc').html($("#payment-desc").html());
+  flicker();
+  $('#pp-sec').hide();
+  $('#ex-filter').hide();
+  $('#exchanges-filter').hide();
+  $('#payment-processors').hide();
+  $('#payment-desc').hide();
+  $('#exchanges-sec').hide();
+  $('#otc-sec').hide();
+  $('#otc-providers').hide();
+  $('#pp-providers').show();
+}
+
+// subtle animation feedback for section change
+function flicker(){
+  $('#exchanges-header')
+      .fadeOut(1)
+      .fadeIn(155);
+  $('#exchanges-desc')
+      .fadeOut(1)
+      .fadeIn(155);
+  $('#pp-providers')
+      .fadeOut(1)
+      .fadeIn(155);
+}
