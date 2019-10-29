9a6526490f3b2bc5d684e3f376ccca113669a7d5
degeri <41839293+degeri@users.noreply.github.com>
2019-10-29 18:03:54 +0530

    Add console message for recruitment (#743)

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -729,3 +729,19 @@ for (var i = 0; i < pathEls.length; i++) {
     autoplay: true
   });
 }
+
+
+var consolestyle = [
+	'background: linear-gradient(to right, #2970ff, #2ED6A1);',
+	'color: #091440',
+	'font-family: monospace',
+	].join(';');
+  
+  console.log(`%c
+  Stakey needs you! for a bug squishin' mission https://docs.decred.org/contributing/overview/
+    ┌ᴗᴗᴗᴗᴗᴗ┐╭  ╮┌ᴗᴗᴗᴗᴗᴗ┐╭  ╮┌ᴗᴗᴗᴗᴗᴗ┐    ┌ᴗᴗᴗᴗᴗᴗ┐╭    ┌ᴗᴗᴗᴗᴗᴗ┐╭  ╮┌ᴗᴗᴗᴗᴗᴗ┐╭  ╮┌ᴗᴗᴗᴗᴗᴗ┐    ┌ᴗᴗᴗᴗᴗᴗ┐╭ 
+   ╭╣● ▄  ●╠╯  ╰╣●    ●╠╯  ╰╣●   ● ╠╮  ╭╣● ▄▄ ●╠╯   ╭╣● ▄▄ ●╠╯  ╰╣●    ●╠╯  ╰╣●   ● ╠╮  ╭╣●  ▄ ●╠╯ 
+   ╯║      ║    ║   ▄  ║    ║  ▄▄  ║╰  ╯║      ║    ╯║      ║    ║  ▄▄  ║    ║  ▄   ║╰  ╯║      ║  
+    ╚─┬──┬─╝    ╚─┬──┬─╝    ╚─┬──┬─╝    ╚─┬──┬─╝     ╚─┬──┬─╝    ╚─┬──┬─╝    ╚─┬──┬─╝    ╚─┬──┬─╝  
+      ┙  ┕        ┕  ┙        ┙  ┙        ┙  ┕         ┙  ┙        ┕  ┕        ┕  ┙        ┙  ┕    `
+  , consolestyle);
