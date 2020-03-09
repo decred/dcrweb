53ef357bb5e60820726e7b69c5a25ab057d3809d
jholdstock <jholdstock@decred.org>
2020-03-09 18:20:09 +0000

    Remove unused anime.js

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -344,25 +344,6 @@ $(document).ready(function () {
 	}
 });
 
-
-var pathEls = document.querySelectorAll('path');
-for (var i = 0; i < pathEls.length; i++) {
-  var pathEl = pathEls[i];
-  var offset = anime.setDashoffset(pathEl);
-  pathEl.setAttribute('stroke-dashoffset', offset);
-  anime({
-    targets: pathEl,
-    strokeDashoffset: [offset, 0],
-    duration: anime.random(1000, 3000),
-    delay: anime.random(0, 2000),
-    loop: true,
-    direction: 'alternate',
-    easing: 'easeInOutSine',
-    autoplay: true
-  });
-}
-
-
 var consolestyle = [
 	'background: linear-gradient(to right, #2970ff, #2ED6A1);',
 	'color: #091440',
