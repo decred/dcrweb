96c02e8e14a545d77989ed34e2eec4346f00d245
jholdstock <jholdstock@decred.org>
2021-09-10 10:18:40 +0100

    Document usage of Hugo binary.
    Use long flag names and include short doco for each one.

--- bin/build-hugo.sh
+++ bin/build-hugo.sh
@@ -7,6 +7,9 @@ bin/i18n-convert-transifex-hugo.sh
 # Remove old hugo output before building
 rm -rf src/public src/resources
 
-mkdir -p src/public src/resources
-
-hugo -F -s src
+# Build site
+#   --buildFuture    include content with publishdate in the future
+#   --source         filesystem path to read files relative from
+hugo \
+    --buildFuture \
+    --source src
--- bin/watch.sh
+++ bin/watch.sh
@@ -2,6 +2,18 @@
 
 bin/i18n-convert-transifex-hugo.sh
 
-hugo --gc -s src
+# Remove old hugo output before building
+rm -rf src/public src/resources
 
-hugo server -F -D --disableFastRender -s src -b http://localhost:1313
+# Serve site
+#   --buildFuture          include content with publishdate in the future
+#   --buildDrafts          include content marked as draft
+#   --disableFastRender    enables full re-renders on changes
+#   --source               filesystem path to read files relative from
+#   --baseURL              hostname (and path) to the root
+hugo server \
+    --buildFuture \
+    --buildDrafts \
+    --disableFastRender \
+    --source src \
+    --baseURL http://localhost:1313
