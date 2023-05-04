b81b0dc8720f40b01f92aaead95d1e64622d541e
Jonathan Chappelow <chappjc@protonmail.com>
2023-05-04 17:35:09 -0500

    update watch.sh to go run hugo@latest

--- bin/watch.sh
+++ bin/watch.sh
@@ -7,13 +7,20 @@ bin/i18n-convert-transifex-hugo.sh
 # Remove old hugo output before building
 rm -rf src/public src/resources
 
+HUGO=hugo
+if [[ ! $(type -P  ${HUGO}) ]]; then
+    HUGO="go run -v -tags extended github.com/gohugoio/hugo@latest"
+fi
+
+${HUGO} version
+
 # Serve site
 #   --buildFuture          include content with publishdate in the future
 #   --buildDrafts          include content marked as draft
 #   --disableFastRender    enables full re-renders on changes
 #   --source               filesystem path to read files relative from
 #   --baseURL              hostname (and path) to the root
-hugo server \
+${HUGO} server \
     --buildFuture \
     --buildDrafts \
     --disableFastRender \
